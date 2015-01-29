432cf6f25cb7cc2ac5d2865e1412fe9390869dbc
Matt Weldon <matt.weldon@gmail.com>
2015-01-29 07:03:05 +0000

    improve tests for sprint api calls

--- test/api/sprints_test.exs
+++ test/api/sprints_test.exs
@@ -7,8 +7,24 @@ defmodule SprintsTest do
     :ok
   end
 
-  test "find by rapid view returns all sprints for the rapid view" do
-    sprints = ExJira.API.Sprints.for_rapid_view(4)
+  test "list by rapid view returns all sprints for the rapid view" do
+    sprint_list = ExJira.API.Sprints.list_for_rapid_view(4)
+    assert sprint_list.rapidViewId == 4
+    assert Enum.count(sprint_list.sprints) > 0
+
+    sprint_summary = List.first(sprint_list.sprints)
+    assert sprint_summary.id == 6
+    assert sprint_summary.name != ""
   end
+
+
+  test "find by rapid view and sprint id returns full sprint info for the sprint associated with the rapid view" do
+    sprint = ExJira.API.Sprints.find_by_rapid_view_and_sprint_id(4, 6)
+
+    assert sprint.contents.allIssuesEstimateSum.text == "50.0"
+    assert Enum.count(sprint.contents.completedIssues) == 0
+    assert Enum.count(sprint.contents.incompletedIssues) > 0
+  end
+
 
 end
